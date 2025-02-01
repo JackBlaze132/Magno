3ff3bc30299bf3746c8dd15ba4481bbd87b7cfc5
ST2712 <2220211016@estudiantesunibague.edu.co>
2025-02-01 10:15:18 -0500

    Delete: RoleEntityMapperimpl unnecessary

--- backend/src/main/java/com/unibague/magno/infrastructure/output/jpa/mapper/RoleEntityMapper.java
+++ backend/src/main/java/com/unibague/magno/infrastructure/output/jpa/mapper/RoleEntityMapper.java
@@ -2,10 +2,13 @@ package com.unibague.magno.infrastructure.output.jpa.mapper;
 
 import com.unibague.magno.domain.model.Role;
 import com.unibague.magno.infrastructure.output.jpa.entity.RoleEntity;
+import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface RoleEntityMapper {
 
     Role toRole(RoleEntity roleEntity);
