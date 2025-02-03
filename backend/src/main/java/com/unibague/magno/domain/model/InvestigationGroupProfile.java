a50cce27e04cc7b147ad94785c0fca699292ec39
ST2712 <2220211016@estudiantesunibague.edu.co>
2025-02-03 15:27:42 -0500

    Add: Basics in igpf model

--- backend/src/main/java/com/unibague/magno/domain/model/InvestigationGroupProfile.java
+++ backend/src/main/java/com/unibague/magno/domain/model/InvestigationGroupProfile.java
@@ -6,4 +6,46 @@ public class InvestigationGroupProfile {
     private Long investigationGroupId;
     private Long coordinatorId;
     private Long academicPeriodId;
+
+    public InvestigationGroupProfile(Long id, Long investigationGroupId, Long coordinatorId, Long academicPeriodId) {
+        this.id = id;
+        this.investigationGroupId = investigationGroupId;
+        this.coordinatorId = coordinatorId;
+        this.academicPeriodId = academicPeriodId;
+    }
+
+    public InvestigationGroupProfile() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getInvestigationGroupId() {
+        return investigationGroupId;
+    }
+
+    public void setInvestigationGroupId(Long investigationGroupId) {
+        this.investigationGroupId = investigationGroupId;
+    }
+
+    public Long getCoordinatorId() {
+        return coordinatorId;
+    }
+
+    public void setCoordinatorId(Long coordinatorId) {
+        this.coordinatorId = coordinatorId;
+    }
+
+    public Long getAcademicPeriodId() {
+        return academicPeriodId;
+    }
+
+    public void setAcademicPeriodId(Long academicPeriodId) {
+        this.academicPeriodId = academicPeriodId;
+    }
 }
