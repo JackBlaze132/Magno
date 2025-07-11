bf3c30ed40bad17daf703c04c1bef74240e13e6a
JackBlaze132 <ederdmc@hotmail.com>
2025-07-10 21:35:56 -0500

    Fix: implemeted null tutor formatter
    Now ypu can create and read seedbeds with null tutor
    Co-authored-by: ST2712 2220211016@estudiantesunibague.edu.co

--- frontend/src/views/academic-periods/investigation-groups/listGroupsByPeriod.vue
+++ frontend/src/views/academic-periods/investigation-groups/listGroupsByPeriod.vue
@@ -1,3 +1,26 @@
+<script setup lang="ts">
+const items = [
+  { title: 'Inicio', disabled: false, to: '/inicio' },
+  { title: 'Períodos Académicos', disabled: false, to: '/periodos' },
+  { title: 'Grupos de Investigación', disabled: true }
+];
+</script>
+
+<template>
+  <VCard variant="flat" color="transparent">
+    <VOverlay :model-value="loading" class="d-flex align-center justify-center" opacity="0.85" persistent contained>
+      <v-progress-circular indeterminate color="primary" size="64"/>
+    </VOverlay>
+    <h1>Grupos de investigación</h1>
+    <v-breadcrumbs :items="items">
+      <template v-slot:divider>
+        <v-icon icon="ri-arrow-right-s-line"></v-icon>
+      </template>
+    </v-breadcrumbs>
+    <table-groups-profiles @loaded="onChildLoeaded"/>
+  </VCard>
+</template>
+
 <script lang="ts">
 import LoadingManager from '@/utils/loadingManager';
 import TableGroupsProfiles from "@/components/tables/groups/tableGroupsProfiles.vue";
@@ -24,11 +47,3 @@ export default{
 }
 
 </script>
-<template>
-  <VCard variant="flat" color="transparent">
-    <VOverlay :model-value="loading" class="d-flex align-center justify-center" opacity="0.85" persistent contained>
-      <v-progress-circular indeterminate color="primary" size="64"/>
-    </VOverlay>
-    <table-groups-profiles @loaded="onChildLoeaded"/>
-  </VCard>
-</template>
