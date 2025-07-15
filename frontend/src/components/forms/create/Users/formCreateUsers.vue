9ed701eb5b3ad77d18f3e9ed72f3508400a7e1a5
JackBlaze132 <ederdmc@hotmail.com>
2025-07-15 16:09:48 -0500

    Fix: Implemented combobox field in user creation"
    Now the create user form has a combobox component to select the type
    instead of a text field

--- frontend/src/components/forms/create/Users/formCreateUsers.vue
+++ frontend/src/components/forms/create/Users/formCreateUsers.vue
@@ -1,6 +1,7 @@
 <template>
   <!-- Reemplaza tu lógica anterior con formCreateGeneral -->
   <formCreateGeneral
+    :v-if="loaded"
     :type="type"
     :fields="fields"
     :name="name"
@@ -11,9 +12,16 @@
 <script lang="ts">
 // ...existing code...
 import { defineComponent } from 'vue';
+import API from "@/utils/api";
 
 export default defineComponent({
   name: 'formCreateUser',
+  data(){
+    return {
+      loaded: false,
+      options: [],
+    }
+  },
   props:{
     name: {
       type: String,
@@ -26,10 +34,36 @@ export default defineComponent({
       type: String,
     }
   },
+  async created() {
+    await this.fetchTypes();
+    this.loaded = true;
+  },
   methods: {
     // ...existing code...
     handleItemCreated() {
       this.$emit('itemCreated');
+    },
+    async fetchTypes() {
+      const headers = {
+        'API-VERSION': '1',
+      };
+      try {
+        const integraTyupes = await API.get(API.INTEGRA_USER_TYPES, headers);
+        this.$emit('loaded');
+        console.log(integraTyupes);
+        console.log("Hola obtuve los roles")
+        //console.log(this.periods)
+
+        const typesField = this.fields.find(f => f.key === 'type')
+        if (typesField) {
+          typesField.options = integraTyupes.map((intType: any) => ({
+            label: intType,
+            value: intType.toUpperCase(),
+          }));
+        }
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
     },
   },
 });
--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -13,6 +13,7 @@ class API{
   public readonly LINES_OF_RESEARCH_BY_INVESTIGATION_GROUP: string='enums/get-lines-of-research-by-investigation-group-id/';
   public readonly LINES_OF_RESEARCH_BY_RESEARCH_SEEDBED: string='enums/get-lines-of-research-by-research-seedbed-id/'
   public readonly LINES_OF_RESEARCH_BY_INVESTIGATION_VALUES: string='enums/LineOfResearch/values';
+  public readonly INTEGRA_USER_TYPES: string='enums/JSONIntegraType/values';
 
   //----[ACADEMIC PERIODS]----
   public readonly ACADEMIC_PERIODS:string='academic-periods/';
