ca375ae55c176c50c7fa7efb034ae44309bc0275
ST2712 <2220211016@estudiantesunibague.edu.co>
2025-08-07 11:16:54 -0500

    Add: Corrections

--- backend/src/main/java/com/unibague/magno/infrastructure/configuration/WebConfig.java
+++ backend/src/main/java/com/unibague/magno/infrastructure/configuration/WebConfig.java
@@ -9,11 +9,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        /*
+
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173", "http://localhost:3000", "https://semilleros.unibague.edu.co") // Change this to the allowed origin
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);*/
+                .allowCredentials(true);
     }
 }
--- backend/src/main/java/com/unibague/magno/infrastructure/configuration/security/SecurityConfig.java
+++ backend/src/main/java/com/unibague/magno/infrastructure/configuration/security/SecurityConfig.java
@@ -60,7 +60,8 @@ public class SecurityConfig {
                             }
                         })
                         .userInfoEndpoint(userInfo -> userInfo.userService(oauth2UserService()))
-                );
+                )
+                .addFilterBefore(googleIdTokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
