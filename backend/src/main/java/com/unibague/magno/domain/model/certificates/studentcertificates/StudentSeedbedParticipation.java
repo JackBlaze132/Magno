be40c51596ef275454b8d9f4690b216710d086d8
ST2712 <2220211016@estudiantesunibague.edu.co>
2025-11-15 13:26:20 -0500

    Fix: StudentSeedbedParticipation model

--- backend/src/main/java/com/unibague/magno/domain/model/certificates/studentcertificates/StudentSeedbedParticipation.java
+++ backend/src/main/java/com/unibague/magno/domain/model/certificates/studentcertificates/StudentSeedbedParticipation.java
@@ -6,11 +6,14 @@ public class StudentSeedbedParticipation {
     private LocalDate startDate;
     private LocalDate endDate;
     private String seedbedCoordinatorName;
+    private String investigationGroupCoordinatorName;
 
-    public StudentSeedbedParticipation(LocalDate startDate, LocalDate endDate, String investigationGroupCoordinatorName) {
+    public StudentSeedbedParticipation(LocalDate startDate, LocalDate endDate,
+                                       String seedbedCoordinatorName, String investigationGroupCoordinatorName) {
         this.startDate = startDate;
         this.endDate = endDate;
-        this.seedbedCoordinatorName = investigationGroupCoordinatorName;
+        this.seedbedCoordinatorName = seedbedCoordinatorName;
+        this.investigationGroupCoordinatorName = investigationGroupCoordinatorName;
     }
 
     public LocalDate getStartDate() {
@@ -35,5 +38,13 @@ public class StudentSeedbedParticipation {
 
     public void setSeedbedCoordinatorName(String seedbedCoordinatorName) {
         this.seedbedCoordinatorName = seedbedCoordinatorName;
+    }
+
+    public String getInvestigationGroupCoordinatorName() {
+        return investigationGroupCoordinatorName;
+    }
+
+    public void setInvestigationGroupCoordinatorName(String investigationGroupCoordinatorName) {
+        this.investigationGroupCoordinatorName = investigationGroupCoordinatorName;
     }
 }
