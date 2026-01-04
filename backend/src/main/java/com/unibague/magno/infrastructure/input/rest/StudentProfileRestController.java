31d5e219277a0da137237f25c09a42dcb23bf32e
ST2712 <2220211016@estudiantesunibague.edu.co>
2026-01-04 00:59:32 -0500

    Add: StudentProfileRestController permissions

--- backend/src/main/java/com/unibague/magno/infrastructure/input/rest/StudentProfileRestController.java
+++ backend/src/main/java/com/unibague/magno/infrastructure/input/rest/StudentProfileRestController.java
@@ -6,6 +6,7 @@ import com.unibague.magno.application.handler.impl.StudentProfileHandler;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -18,30 +19,35 @@ public class StudentProfileRestController {
 
     private final StudentProfileHandler studentProfileHandler;
 
+    @PreAuthorize("hasRole(T(com.unibague.magno.domain.model.enums.SeedbedRole).DIRI)")
     @GetMapping(path = "/{id}", headers = "API-VERSION=1")
     public ResponseEntity<StudentProfileResponse> getStudentProfileById(@PathVariable Long id) {
         StudentProfileResponse response = studentProfileHandler.findById(id);
         return ResponseEntity.ok(response);
     }
 
+    @PreAuthorize("hasRole(T(com.unibague.magno.domain.model.enums.SeedbedRole).DIRI)")
     @GetMapping(path = "/", headers = "API-VERSION=1")
     public ResponseEntity<List<StudentProfileResponse>> getAllStudentProfiles() {
         List<StudentProfileResponse> responses = studentProfileHandler.findAll();
         return ResponseEntity.ok(responses);
     }
 
+    @PreAuthorize("hasRole(T(com.unibague.magno.domain.model.enums.SeedbedRole).ESTUDIANTE)")
     @GetMapping(path = "/find-all-profiles/{userId}", headers = "API-VERSION=1")
     public ResponseEntity<List<StudentProfileResponse>> getAllStudentProfilesByUserId(@PathVariable Long userId) {
         List<StudentProfileResponse> responses = studentProfileHandler.findAllProfilesByUserId(userId);
         return ResponseEntity.ok(responses);
     }
 
+    @PreAuthorize("hasRole(T(com.unibague.magno.domain.model.enums.SeedbedRole).DIRI)")
     @GetMapping(path = "/academic-period/{academicPeriodId}", headers = "API-VERSION=1")
     public ResponseEntity<List<StudentProfileResponse>> getAllStudentProfilesByAcademicPeriodId(@PathVariable Long academicPeriodId) {
         List<StudentProfileResponse> responses = studentProfileHandler.findAllProfilesByAcademicPeriodId(academicPeriodId);
         return ResponseEntity.ok(responses);
     }
 
+    @PreAuthorize("hasRole(T(com.unibague.magno.domain.model.enums.SeedbedRole).DIRI)")
     @PostMapping(path = "/", headers = "API-VERSION=1")
     public ResponseEntity<StudentProfileResponse> createStudentProfile
             (@Valid @RequestBody StudentProfileRequest studentProfileRequest) {
@@ -50,6 +56,7 @@ public class StudentProfileRestController {
         return ResponseEntity.created(location).body(created);
     }
 
+    @PreAuthorize("hasRole(T(com.unibague.magno.domain.model.enums.SeedbedRole).DIRI)")
     @PutMapping(path = "/{id}", headers = "API-VERSION=1")
     public ResponseEntity<StudentProfileResponse> updateStudentProfileById
             (@PathVariable Long id, @Valid @RequestBody StudentProfileRequest studentProfileRequest) {
@@ -57,6 +64,7 @@ public class StudentProfileRestController {
         return ResponseEntity.ok(updated);
     }
 
+    @PreAuthorize("hasRole(T(com.unibague.magno.domain.model.enums.SeedbedRole).DIRI)")
     @DeleteMapping(path = "/{id}", headers = "API-VERSION=1")
     public ResponseEntity<Void> deleteStudentProfileById(@PathVariable Long id) {
         studentProfileHandler.deleteById(id);
